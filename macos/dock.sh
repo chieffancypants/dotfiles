a06ec5fe3b82f5c96881e6441a0b9c156b54c2b6
Wes Cruver <chieffancypants@gmail.com>
2022-03-14 15:42:33 -0700

    Removed all bash config and system files

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/System/Applications/Mail.app"
+dockutil --no-restart --add "/Applications/Brave Browser.app"
+dockutil --no-restart --add "/Applications/Messages.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
-dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
-dockutil --no-restart --add "/System/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Spotify.app"
-
 killall Dock
